b390f0e532fdfef0794fcecd703ec8c13f182201
Rohan Mehta <rmehta@unicef.org>
2017-01-06 16:56:11 +0000

    Updated style according to PEP 8 and secured account info

--- probe.py
+++ probe.py
@@ -1,22 +1,48 @@
-"""
-This is just various functions that explore the azure blob database
-"""
+#
+# This is just various functions that explore the azure blob database
+#
 
 import sys
-import os
 from azure.storage.blob.baseblobservice import BaseBlobService as bsb
 
-bs = bsb(account_name = ACCOUNT_NAME, account_key = ACCOUNT_KEY)
+#
+# This function gets account name and key from an external file.
+# account_num is the ordered number of the account, starting from 0
+#
 
-def printBlobs(container, blob_service = bs):
-	for blob in blob_service.list_blobs(container):
-		print blob.name
 
-def printContainers(j = 5):
-	for blob in bs.list_containers():
-		print blob.name
+def getAccount(account_num):
+    account_path = "./accounts.txt"  # same directory as this program
+    account_attr = 2  # This is how many lines are used for each account
+    start = account_num * account_attr
+    end = start + account_attr
+    with open(account_path) as inFile:
+        [name, key] = inFile.readlines()[start:end]  # readlines() is fine here
+    return (name, key)
 
-def main(arg):
-	printBlobs(arg)
 
-main(sys.argv[1])
+def printBlobs(container, blob_svc):
+    for blob in blob_svc.list_blobs(container):
+        print blob.name
+
+
+def printContainers(blob_svc):
+    for blob in blob_svc.list_containers():
+        print blob.name
+
+
+def main(category, cont_name="bra", account_num=0):
+    (name, key) = getAccount(account_num)
+    bs = bsb(account_name=name, account_key=key)
+    if category.upper == "B":
+        print "All blobs in account %s, container %s:" % (name, cont_name)
+        printBlobs(cont_name, bs)
+    elif category.upper == "C":
+        print "All containers in account %s:" % name
+        printContainers(bs)
+    else:
+        print "Invalid category input. Enter B or C to list blobs or \
+        categories, respectively. If listing blobs, also enter name of blob"
+
+
+main(sys.argv[1:])
